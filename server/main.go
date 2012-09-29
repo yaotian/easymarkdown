6d229266b10d5ae0e1bef998916d2075202a9898
yaotian <yaotian@gmail.com>
2012-09-29 15:15:50 +0800

    index part, /open/file, /admin

--- server/main.go
+++ server/main.go
@@ -1,9 +1,5 @@
 /**
- * Created with IntelliJ IDEA.
- * User: yaotian
- * Date: 12-9-22
- * Time: 下午7:06
- * To change this template use File | Settings | File Templates.
+ *  This is the easy markdown server
  */
 package main
 
@@ -13,15 +9,42 @@ import (
 	"strings"
 	"io/ioutil"
 	"os"
+	"html/template"
 )
 
-type EasyMarkdownServer struct {
+type EasyServer struct {
 	folder string
 }
 
-func (server EasyMarkdownServer) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	fmt.Println("folder:",server.folder)
+func (server *EasyServer) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	 if r.URL.Path == "/" {
+        index(w,r,server.folder)
+        return
+    }
 
+	 if r.URL.Path == "/admin" {
+        admin(w,r,server.folder)
+        return
+    }
+    
+    if strings.HasPrefix(r.URL.Path,"/open/") {
+    	fmt.Printf("%q\n",strings.Split(r.URL.Path,"/open/")[1])
+    	contents,_ := ioutil.ReadFile(server.folder+"/"+strings.Split(r.URL.Path,"/open/")[1])
+    	fmt.Printf("contents"+string(contents))
+    	fmt.Fprint(w,string(contents))
+    	return
+    }
+    
+    http.NotFound(w, r)
+    return
+}
+
+
+func admin(w http.ResponseWriter, r *http.Request,folder string){
+	fmt.Fprint(w,"admin")
+}
+
+func index(w http.ResponseWriter, r *http.Request,folder string){
 	r.ParseForm()  //解析参数，默认是不会解析的
 	fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
 	fmt.Println("path", r.URL.Path)
@@ -32,7 +55,7 @@ func (server EasyMarkdownServer) ServeHTTP(w http.ResponseWriter, r *http.Reques
 		fmt.Println("val:", strings.Join(v, ""))
 	}
 
-	fi, err := ListDir(server.folder)
+	fi, err := ListDir(folder)
 	if err != nil {
 		fmt.Println("Error", err)
 	}
@@ -40,7 +63,7 @@ func (server EasyMarkdownServer) ServeHTTP(w http.ResponseWriter, r *http.Reques
 	for _, f := range fi {
 		//		d := "-"
 		if f.IsDir() { continue }
-		result += f.Name()+"<br>"
+		result += "<a href=/open/"+template.HTMLEscapeString(f.Name())+">"+template.HTMLEscapeString(f.Name())+"</a><br>"
 	}
 
 	fmt.Fprint(w,result)
@@ -53,20 +76,17 @@ func ListDir(dir string) ([]os.FileInfo, error) {
 
 
 func main() {
-	var server EasyMarkdownServer
 	var port ="9009"
-	server.folder="."
-
+	es := &EasyServer{"."}
 	if len(os.Args) > 1 {
 		if x := strings.Join(os.Args[1:2], ""); x != "" {
 			port = x
 		}
 
 		if x := strings.Join(os.Args[2:3], ""); x != "" {
-			server.folder = x
+			es.folder = x
 		}
 	}
-
-	http.ListenAndServe("localhost:"+port,server)
+	http.ListenAndServe("0.0.0.0:"+port,es)
 }
 
